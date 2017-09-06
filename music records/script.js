5068aabb34130ce2174cad3bb94db2f2513b6b59
Ruta Venckeviciene <30872194+ruthvenc@users.noreply.github.com>
2017-09-06 21:28:00 +0300

    something new

--- music records/script.js
+++ music records/script.js
@@ -27,7 +27,8 @@ addAlbumButton.addEventListener("click", addAlbum);
 
 
 // Čia saugosim visus albumus
-let albumList = [];
+let albumsJSON = localStorage.getItem("albums");
+let albums = JSON.parse(albumsJSON);
 
 // Formos elementai
 let artistElement = document.querySelector("#artist");
@@ -56,7 +57,7 @@ function addAlbum() {
     }
 
     // Patikrinime ar albumas jau buvo įvestas
-    if (checkForDuplicates(record)){
+    if (checkForDuplicates(record)) {
         alert("Toks albumas jau išsaugotas");
         return;
     }
@@ -64,18 +65,67 @@ function addAlbum() {
     // Saugome albumą
     albumList.push(record);
 
-    // 
+    // ivestus duomenis issaugosime i LocalStorage
+    let albumsJSON = JSON.stringify(albumList);
+    localStorage.setItem("albums", albumsJSON);
 
 
-    console.log("Duomenys:", artist, album, date, image);    
+
+    console.log("Duomenys:", artist, album, date, image);
 }
 
 function checkForDuplicates(record) {
     let isDuplicate = false;
-    albumList.forEach(function(item){
+    albumList.forEach(function (item) {
         if (item.artist === record.artist && item.album === record.album && item.date === record.date) {
             isDuplicate = true;
         }
     })
     return isDuplicate;
 };
+
+//duomenu atvaizdavimas
+let albumListElement = document.querySelector(".album-list");
+
+let result = `
+    <div class="album">
+        <img src="img/R-365109-1390519203-2623.jpeg.jpg" alt="Chemical Brothers - Push the button">
+        <h2>Chemical Brothers <span> Push the button</span></h2>
+        <date>2004</date>
+    </div>
+`;
+function renderAlbums() {
+
+    // gauname albumus is Local Storage ir sukuriame objekta
+    let albumsJSON = localStorage.getItem("albums");
+    let albums = JSON.parse(albumsJSON);
+
+    //patikrinti ar yra issaugotu albumu
+    //jei nera, nutraukti funkcijos vykdyma
+    if (!albums) return;
+
+    //jei yra, kuriame cikla ir i rezultata susidedam visu albumu HTML
+    let result = ""
+    for (let album of albums) {
+        result = `
+            <div class="album">
+            <img src="img/${album.image}" alt="${album.artist} - ${album.album}">
+            <h2>${album.album}<span>${album.artist}</span></h2>
+            <date>${album.date}</date>
+            </div>
+            <hr>
+        `;
+
+    }
+    //duomenu atvaizdavimas
+    //spausdiname rezultata i ".album-list"
+    let albumListElement = document.querySelector(".album-list");
+    albumListElement.innerHTML = result;
+}
+
+
+renderAlbums();
+
+albumListElement.innerHTML = result;
+
+

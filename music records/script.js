9c096c90f4ad170bce97b14a44a5b8a8a1aef1b2
Ruta Venckeviciene <30872194+ruthvenc@users.noreply.github.com>
2017-09-06 19:56:50 +0300

    made some changes

--- music records/script.js
+++ music records/script.js
@@ -1,55 +1,81 @@
-//Suraskime DOM elementa pagal klase ir priskirkite ji kintamajam
+// Suraskite H1 DOM elementą pagal klasę ir priskirkime jį kintamąjam
 let titleElement = document.getElementsByClassName("main-title")[0];
 
-//pakeiskime h1 title pavadinima
+
+// Pakeiskime H1 title pavadinima
 titleElement.innerHTML = "Muzikos albumai";
 
-//pridekime papildoma klase
+
+// Pridėkime padildomą klasę H1
 titleElement.classList.add("header-title");
 
-//pakeiskime h1 stiliu
+
+// Pakeiskime stiliųs
 titleElement.style.fontStyle = "italic";
 
-//Surandame fromos mygtuka "Prideti"
-//Suras pirma DOM elementa su klase btn-save-album
+
+
+// Surandame formos mygtuką "Pridėti"
+// suras pirmą DOM elementą su klase .btn-save-album
 let addAlbumButton = document.querySelector(".btn-save-album");
 
-//Kai paspaudziamas mygtukas, vykdys addAlbum funkcija
+// metodas .querySelectorAll() suranda visus elementus su klase .btn-save-album
+// let addAlbumButton = document.querySelectorAll(".btn-save-album");
+
+// Kai paspaudžiamas mygtukas, vykdyk addAlbum funkciją
 addAlbumButton.addEventListener("click", addAlbum);
 
-// cia bus saugomas elementas
+
+// Čia saugosim visus albumus
 let albumList = [];
 
-//formos elementai
+// Formos elementai
 let artistElement = document.querySelector("#artist");
 let albumElement = document.querySelector("#album");
 let releaseDateElement = document.querySelector("#releaseDate");
 let imageElement = document.querySelector("#image");
 
-function addAlbum(){
-//nuskaitome laukeliu reiksmes
-let artist = artistElement.value;
-let album = albumElement.value;
-let date = releaseDateElement.value;
-let image = imageElement.files[0];
 
-if (!artist || !album || !date || !image) {
-    console.log("Neivestas Miau atlikejas");
-    return;
+function addAlbum() {
+    // nuskaitome laukelių reikšmes
+    let artist = artistElement.value;
+    let album = albumElement.value;
+    let date = releaseDateElement.value;
+    let image = imageElement.files[0];
+
+    if (!artist || !album || !date || !image) {
+        console.log("Neįvesti laukeliai");
+        return;
+    }
+
+    let record = {
+        "artist": artist,
+        "album": album,
+        "date": date,
+        "image": image.name
+    }
+
+    // Patikrinime ar albumas jau buvo įvestas
+    if (checkForDuplicates(record)){
+        alert("Toks albumas jau išsaugotas");
+        return;
+    }
+
+    // Saugome albumą
+    albumList.push(record);
+
+    // 
+
+
+    console.log("Duomenys:", artist, album, date, image);    
 }
 
-
+function checkForDuplicates(record) {
+    let isDuplicate = false;
+    albumList.forEach(function(item){
+        if (item.artist === record.artist && item.album === record.album && item.date === record.date) {
+            isDuplicate = true;
+        }
+    })
+    return isDuplicate;
 };
-
-albumList.push({
-    "artist":artist,
-    "album": album,
-    "date": date,
-    "image": image.name
-});
-
-//    console.log("Pridedam albuma");
-
-console.log("Duomenys:", artist, album, date, image);
-}
-
